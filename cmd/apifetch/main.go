29e50bf3ba0ab03fba781eebbc71b91cdbfc0653
Ragnar Wessels <ragnarwessels@gmail.com>
2026-08-27 15:07:51 +0200

    Revert "chore: add --body-file flag to apifetch"
    This reverts commit 617d69d6068f5ddd87d3a52c99af777c2b5cea6a.

--- cmd/apifetch/main.go
+++ cmd/apifetch/main.go
@@ -15,7 +15,6 @@ const defaultBaseURL = "https://api.cyberspace.online"
 func main() {
 	method := flag.String("method", "GET", "HTTP method")
 	body := flag.String("body", "", "request body (JSON string)")
-	bodyFile := flag.String("body-file", "", "request body (path to JSON file; avoids shell quoting issues)")
 	baseURL := flag.String("base-url", "", "override API base URL")
 	flag.Parse()
 
@@ -50,14 +49,7 @@ func main() {
 	}
 
 	var bodyBytes []byte
-	if *bodyFile != "" {
-		b, err := os.ReadFile(*bodyFile)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "error: read body file: %v\n", err)
-			os.Exit(1)
-		}
-		bodyBytes = b
-	} else if *body != "" {
+	if *body != "" {
 		bodyBytes = []byte(*body)
 	}
 
